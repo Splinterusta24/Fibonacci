f87599f21d7ac94f6607b63f6c6082fcc9484cf8
2022-01-001 <2022-01-001@DESKTOP-63E5VH2>
2022-04-22 22:50:44 +0300

    Add project files.

---
+++ Hesap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonnacci
+{
+    internal class Hesap
+    {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        public decimal hesaplama()
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            return b;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonnacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Decimal> fibo = new List<Decimal>();
+            Hesap x = new Hesap();
+
+            decimal ort = 0;
+            decimal sonuc = 0;
+            decimal eleman;
+
+            Console.WriteLine("Derinlik ne kadar olsun?");
+            int sayi = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sayi; i++)
+            {
+                fibo.Add(x.hesaplama());
+            }
+            Console.Write("1,");
+            Console.Write(string.Join(',', fibo.ToArray()));
+            for (int i = 0; i < sayi; i++)
+            {
+                ort += fibo[i];
+                sonuc = Queryable.Average(fibo.AsQueryable());
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("ortalama: " + sonuc);
+
+
+        }
+    }
+}
